b16d46cd207f443693cbca42640750be630c0b13
Miroslav Ilic <ilic.miroslav1993@gmail.com>
2023-11-02 00:37:13 +0100

    Added Command service, http client for communication between services, set up containers in docker and set up pods in kubernetes and tested them

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,9 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using PlatformService;
-using PlatformService.Repositories;
-using PlatformService.SyncDataServices.Http;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,10 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
-builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -30,6 +20,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-PrepDB.PrepPopulation(app);
 app.MapControllers();
+
 app.Run();
